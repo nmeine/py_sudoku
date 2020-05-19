eddce1b388cdaba7edae517445596f9d0bbaa320
Noah Meine <usaswiss@vt.edu>
2020-05-19 19:53:58 -0400

    Added indexing

--- app_class.py
+++ app_class.py
@@ -1,6 +1,7 @@
 import pygame
 import settings
 import sys
+import math
 
 
 class App():
@@ -10,8 +11,9 @@ class App():
         self.window = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
         self.running = True
         self.grid = settings.testBoard
-        #print(self.grid)
         self.first_call = True
+        self.selected = None
+        self.mouse_position = None
 
     def run(self):
         while self.running:
@@ -25,10 +27,16 @@ class App():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if(self.mouse_on_grid()):
+                    self.selected = self.get_mouse_index()
+                    print(self.selected)
+                else:
+                    self.selected = None
         pass
 
     def update(self):
-        pass
+        self.mouse_position = pygame.mouse.get_pos()
 
     def draw(self):
         if self.first_call:
@@ -36,6 +44,8 @@ class App():
             self.window.fill(settings.WHITE)
             self.draw_grid(self.window)
             pygame.display.update()
+        elif self.selected:
+            self.draw_selection(self.window, self.selected)
         pass
 
     def draw_grid(self, window):
@@ -54,4 +64,23 @@ class App():
             pygame.draw.line(window, settings.BLACK,
                              (settings.gridPos[0], settings.gridPos[1] + (x * settings.cellSize)),
                              (settings.gridPos[0] + 450, settings.gridPos[1] + (x * settings.cellSize)), thickness)
+
+    def mouse_on_grid(self):
+        if settings.XMIN <= pygame.mouse.get_pos()[0] <= settings.XMAX and settings.YMIN <= pygame.mouse.get_pos()[1] <= settings.YMAX:
+            return True
+        else:
+            return False
+
+    def get_mouse_index(self):
+        index = []
+        x_pos = pygame.mouse.get_pos()[0] - settings.XMIN
+        x_index = math.floor(x_pos / settings.cellSize)
+        index.append(x_index)
+        y_pos = pygame.mouse.get_pos()[1] - settings.YMIN
+        y_index = math.floor(y_pos / settings.cellSize)
+        index.append(y_index)
+        return index
+
+    def draw_selection(self, window, pos):
+        pass
 
